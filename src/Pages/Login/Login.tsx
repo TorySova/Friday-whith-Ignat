04b2dee9beb05a7500f19e52e4d20f3173ae8119
TorySova <toryssova@gmail.com>
2020-12-14 15:18:47 +0200

    test

--- src/Pages/Login/Login.tsx
+++ src/Pages/Login/Login.tsx
@@ -4,8 +4,9 @@ import SuperInput from '../../SuperComponent/SuperInput/SuperInput'
 export const Login = () => {
     return (
         <div>
-            <SuperInput/>
-            <SuperInput/>
+            <SuperInput type={'text'}/>
+            <SuperInput type={'password'}/>
+            <input type="checkbox"/>
             <button>login</button>
         </div>
     )
