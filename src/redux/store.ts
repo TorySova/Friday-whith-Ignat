7921a9a3bf13589a58adb6d90ee05f5471bac945
Maximone23 <maxim.one23@gmail.com>
2020-12-19 16:17:19 +0500

    Create UI for NewPassword page

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -2,11 +2,12 @@ import { applyMiddleware, combineReducers, createStore } from "redux";
 import thunkMiddleware from 'redux-thunk';
 import {profileReducer} from "./profileReducer";
 import {forgotReducer} from "./forgotReducer";
+import {setPassReducer} from "./setPasswordReducer";
 
 const reducers = combineReducers({
     profile: profileReducer,
-    forgot: forgotReducer
-
+    forgot: forgotReducer,
+    setPassword: setPassReducer
 });
 
 const store = createStore(reducers, applyMiddleware(thunkMiddleware));
