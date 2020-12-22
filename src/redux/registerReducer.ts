b890fcdba250a78c4e65860925cb5d34bfb72f70
AndreiLeonov <andrei_leonov93@mail.ru>
2020-12-22 18:37:36 +0300

    refactoring

--- src/redux/registerReducer.ts
+++ src/redux/registerReducer.ts
@@ -16,30 +16,24 @@ export const registerReducer = (state: InitialStateType = initialState, action: 
     }
 }
 
-// actions
-// export const createUser = (newUser: any) =>
-//     ({type: 'REGISTER-REDUCER/CREATE-USER', newUser} as const)
-
 export const isRegisteredAC = (isRegistered: boolean) =>
     ({type: 'REGISTER-REDUCER/IS-REGISTERED-AC', isRegistered} as const)
 
 // thunks
 export const registerNewUserTC = (newUserData: regNewUserDataType) => (dispatch: any) => {
+    //добавить крутилку + дисейбл кнопки (не забыть убрать!)
     registerAPI.regNewUser(newUserData)
         .then( response => {
             if (response.status === 201) {
                 dispatch(isRegisteredAC(true));
-            } else {
-                dispatch(isRegisteredAC(false));
-                alert(response.status);
             }
         })
         .catch( (error) => {
-            alert(error)
+            //сделать какой-нибудь popup для ошибок
+            alert(error.response.data.error);
         })
 }
 
 // types
 type ActionsType =
     | ReturnType<typeof isRegisteredAC>
-  //| ReturnType<typeof createUser>
